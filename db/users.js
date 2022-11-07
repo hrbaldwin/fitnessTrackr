4adb0cec800a3aa508e732ebc716ef775caafbdc
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-11-07 12:57:35 -0600

    first commit

--- db/users.js
+++ db/users.js
@@ -4,24 +4,30 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  const {
+    rows: [user],
+  } = await client.query(
+    `
+        INSERT INTO users(username, password) 
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+        `,
+    [username, password]
+  );
+
+  return user;
 }
 
-async function getUser({ username, password }) {
+async function getUser({ username, password }) {}
 
-}
+async function getUserById(userId) {}
 
-async function getUserById(userId) {
-
-}
-
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
