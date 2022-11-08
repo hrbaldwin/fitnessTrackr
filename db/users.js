6fe503bed189c69742b76bfeda6345d8a9a680f2
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-11-08 10:44:05 -0600

    worked on getUserById and started on getUser

--- db/users.js
+++ db/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 const client = require("./client");
 
 // database functions
@@ -19,21 +20,44 @@ async function createUser({ username, password }) {
   return user;
 }
 
-async function getUser({ username, password }) {}
+async function getUser({ username, password }) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        SELECT username, password
+        FROM users
+        WHERE password;
+    `,
+      [username, password]
+    );
 
-async function getUserById(userId) {
-  try{
-  const {
-    rows: [user],
-  } = await client.query(`
-    SELECT username, password
-    FROM users
-    WHERE id = ${userId}
-    `);
-    return user; 
-  }catch(error){
+    return user;
+  } catch (error) {
     throw error;
   }
+}
+
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+        SELECT id, username
+        FROM users
+        WHERE "id"=${userId};
+  `);
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getUserByUsername(userName) {}
 
 module.exports = {
