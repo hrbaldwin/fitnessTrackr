7b23e3b60df22713e6e2cbdbc7e4a5808c337ed6
Randy Pellegrin <rpell391@gmail.com>
2022-11-08 10:14:54 -0600

    added getUserById

--- db/users.js
+++ db/users.js
@@ -21,8 +21,19 @@ async function createUser({ username, password }) {
 
 async function getUser({ username, password }) {}
 
-async function getUserById(userId) {}
-
+async function getUserById(userId) {
+  try{
+  const {
+    rows: [user],
+  } = await client.query(`
+    SELECT username, password
+    FROM users
+    WHERE id = ${userId}
+    `);
+    return user; 
+  }catch(error){
+    throw error;
+  }
 async function getUserByUsername(userName) {}
 
 module.exports = {
