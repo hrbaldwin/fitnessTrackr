87e60d435317382d47e3d2674e40e53375ece920
Randy Pellegrin <rpell391@gmail.com>
2022-11-10 10:01:20 -0600

    setting up api

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { getAllActivities } = require("../db");
 
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
+router.get("/", async (req, res, next) => {
+    try{
+        const allActivities =
+    } catch ({name, message}){
+        
+    }
+})
 
 // POST /api/activities
 
--- api/users.js
+++ api/users.js
@@ -6,6 +6,32 @@ const { JWT_SECRET } = process.env;
 const { createUser, getUserByUsername } = require("../db");
 
 // POST /api/users/login
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+  try {
+    const user = await getUserByUsername(username);
+    if (user && user.password == password) {
+      const token = jwt.sign({ id: user.id, username }, JWT_SECRET);
+      res.send({ message: "you're logged in!", token });
+      return token;
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // POST /api/users/register
 router.post("/register", async (req, res, next) => {
@@ -25,7 +51,7 @@ router.post("/register", async (req, res, next) => {
 
     const token = jwt.sign(
       {
-        id: creatingUser.id,
+        id: user.id,
         username,
       },
       process.env.JWT_SECRET,
